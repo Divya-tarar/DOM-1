b8331d93f12f7deb0a50236d3442665b9844a903
Divya Tarar <tarardivya09@gmail.com>
2026-02-03 22:59:06 +0530

    callback code

--- script3.js
+++ script3.js
@@ -12,6 +12,29 @@
 //     count+=1
 
 // },1000
+console.log("Starting homework...");
+
+setTimeout(() => {
+
+console.log ("Homework done!");
+
+console.log("Starting dinner...");
+
+setTimeout(() => {
+
+console.log("Dinner done!");
+
+console.log("Getting ready to go out...");
+
+setTimeout(() => {
+
+console.log("Going to the playground!");
+
+}, 1000); // after dinner
+
+}, 1500); // dinner time
+
+}, 2000); // homework time
 
 const p=new Promise ((resizeBy,rej)=>{
     let done=false
